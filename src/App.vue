aae7ceda4023dc034d83f498a5e792fdaf737272
Abdullah <shaikhabdullah299@gmail.com>
2022-02-19 23:00:05 +0530

    revert some stylying

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<signup/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import signup from './components/signup.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    signup
   }
 }
 </script>
@@ -22,5 +21,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  background: #2c3e50;
+  margin: 0;
 }
 </style>
--- src/components/signup.vue
+++ src/components/signup.vue
@@ -25,7 +25,7 @@ label{
     font-weight: bold;
     font-size: 1.5em;
     font-family: 'Times New Roman';
-    color: rgb(161, 161, 161);
+    color: #2c3e50;
     margin: 25px 0 15px;
     text-transform: uppercase;
     letter-spacing: 1px;
@@ -36,7 +36,7 @@ input{
     width: 100%;
     box-sizing: border-box;
     border: none;
-    border-bottom: 2px solid rgb(206, 206, 206);
+    border-bottom: 1px solid rgb(206, 206, 206);
     color: #555;  
 }
 </style>
