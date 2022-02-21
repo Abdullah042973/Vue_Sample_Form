1066de9e159a8009c33d60d49ac797cd886e1217
Abdullah <shaikhabdullah299@gmail.com>
2022-02-21 16:26:51 +0530

    form changes 1.2

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,13 +1,47 @@
 <template>
   <form>
       <label>Email</label>
-      <input type="email" v-model="email">
+      <input type="email" required v-model="email">
 
       <label>Password</label>
       <input type="password" v-model="password">
+
+      <label> Role:</label>
+      <select v-model="role">
+          <option value="designer">Web designer</option>
+          <option value="developer">Web developer</option>
+          <option value="hosting">Web hosting</option>
+      </select>
+
+    <label>Education:</label>
+      <div>
+          <input type="checkbox" value="SSC" v-model="names">
+          <label>SSC</label>
+      </div>
+      <div>
+          <input type="checkbox" value="HSC" v-model="names">
+          <label>HSC</label>
+      </div>
+      <div>
+          <input type="checkbox" value="Graduation" v-model="names">
+          <label>Graduation</label>
+      </div>
+
   </form>
+
+  <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Terms and Condition</label>
+  </div>
+
+  <div class="show">
   <p>email: {{ email }}</p>
   <p>password: {{ password }}</p>
+  <p>Role: {{ role }} </p>
+  <p>Terms: {{ terms }}</p>
+  <p>Levels: {{ names }}</p>
+  </div>
+
 </template>
 
 <script>
@@ -15,7 +49,10 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            role: '',
+            terms: '',
+            names: []
     }
 }
 }
@@ -40,16 +77,45 @@ label{
     text-transform: uppercase;
     letter-spacing: 1px;
 }
-input{
+input, select{
     display: block;
     padding: 10px 6px;
     width: 100%;
     box-sizing: border-box;
     border: none;
     border-bottom: 1px solid rgb(160, 160, 160);
-    color: #555;  
+    color: #555;
+    font-weight: bold;
+    font-size: 0.8em;
+    font-family: 'Times New Roman';
+    letter-spacing: 1px;
+    caret-color: rgb(255, 93, 93);  
 }
 p{
-    color: white;
+    display: block;
+    font-weight: bold;
+    font-size: 0.7em;
+    font-family: 'Times New Roman';
+    color: rgb(102, 101, 101);
+    margin: 25px 0 15px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
 }
+.show{
+    max-width: 450px;
+    background: white;
+    color: rgb(214, 235, 253);
+    padding: 40px;
+    border-radius: 10px;
+    margin: 10px auto;
+    text-align: left;
+}
+input[type='checkbox']{
+    display: inline-block;
+    width: 16px;
+    position: relative;
+    margin: 0 10px 0 0;
+    top: 2px;
+}
+
 </style>
